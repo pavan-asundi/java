350bcf37e9f586bd245f138b4762fe5b1de1a7dc
Pavan <pavankumar.asundi@netapp.co>
2020-04-02 16:20:57 +0530

    Test commmit

--- simplemodule/src/Hello.java
+++ simplemodule/src/Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main (String[] args){
         System.out.println("Hello.. Pavan!!");
+        Hello h = new Hello();
+        h.returnNone();
+    }
+
+    public void returnNone(){
+        return;
     }
 }
